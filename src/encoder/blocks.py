bb67d2e32c5bcde1e8eebf2cc546e03286b97301
soumik12345 <19soumik.rakshit96@gmail.com>
2020-06-06 23:53:22 +0530

    Added Residual Connection

--- src/encoder/blocks.py
+++ src/encoder/blocks.py
@@ -14,3 +14,16 @@ class LayerNorm(torch.nn.Module):
         std = x.std(-1, keepdim=True)
         x = self.a * (x - mean) / (std + self.epsilon) + self.b
         return x
+
+
+class ResidualConnection(torch.nn.Module):
+
+    def __init__(self, size, dropout):
+        super(ResidualConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        sublayer_output = sublayer(self.norm(x))
+        sublayer_output = self.dropout(sublayer_output)
+        return x + sublayer_output
