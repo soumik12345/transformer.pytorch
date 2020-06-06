3c1a8ab4e0badf1f437751cdf73f34a8d937ac69
soumik12345 <19soumik.rakshit96@gmail.com>
2020-06-07 01:21:07 +0530

    Added Decoder

---
+++ src/decoder/__init__.py
@@ -0,0 +1,15 @@
+import torch
+from ..utils import clones, LayerNorm
+
+
+class Decoder(torch.nn.Module):
+
+    def __init__(self, layer, n):
+        super(Decoder, self).__init__()
+        self.layers = clones(layer, n)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, source_mask, target_mask):
+        for layer in self.layers:
+            x = layer(x, memory, source_mask, target_mask)
+        return self.norm(x)
