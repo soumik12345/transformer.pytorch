f9588e606471cb9e39d3d94e3c5f8ed3f6785839
soumik12345 <19soumik.rakshit96@gmail.com>
2020-06-08 00:07:06 +0530

    Added Transformer

--- src/model.py
+++ src/model.py
@@ -1,4 +1,11 @@
 import torch
+from copy import deepcopy
+from .attention import MultiHeadedAttention
+from .blocks import PositionWiseFeedForward, PositionalEncoding, Embeddings
+from .encoder import Encoder
+from .decoder import Decoder
+from .encoder.blocks import EncoderLayer
+from .decoder.blocks import DecoderLayer
 
 
 class EncoderDecoder(torch.nn.Module):
@@ -35,3 +42,36 @@ class Generator(torch.nn.Module):
 
     def forward(self, x):
         return torch.nn.functional.log_softmax(self.linear(x), dim=-1)
+
+
+def Transformer(source_vocab, target_vocab, n=6, d_model=512, d_ff=2048, h=8, dropout=0.1):
+    attention = MultiHeadedAttention(h, d_model)
+    feed_forward = PositionWiseFeedForward(d_model, d_ff, dropout)
+    encoding = PositionalEncoding(d_model, dropout)
+    model = EncoderDecoder(
+        Encoder(
+            EncoderLayer(
+                d_model, deepcopy(attention),
+                deepcopy(feed_forward), dropout
+            ), n
+        ),
+        Decoder(
+            DecoderLayer(
+                d_model, deepcopy(attention),
+                deepcopy(attention), deepcopy(feed_forward), dropout
+            ), n
+        ),
+        torch.nn.Sequential(
+            Embeddings(d_model, source_vocab),
+            deepcopy(encoding)
+        ),
+        torch.nn.Sequential(
+            Embeddings(d_model, target_vocab),
+            deepcopy(encoding)
+        ),
+        Generator(d_model, target_vocab)
+    )
+    for p in model.parameters():
+        if p.dim() > 1:
+            torch.nn.init.xavier_uniform(p)
+    return model
