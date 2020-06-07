60f6d15ba707000a47f15d97ed7d2d8c60faca4d
soumik12345 <19soumik.rakshit96@gmail.com>
2020-06-08 00:30:01 +0530

    Added Train Step

--- src/training.py
+++ src/training.py
@@ -1,4 +1,5 @@
 import torch
+from time import time
 from .utils import subsequent_mask
 
 
@@ -6,11 +7,11 @@ class Batch:
 
     def __init__(self, source, target=None, pad=0):
         self.source = source
-        self.src_mask = (source != pad).unsqueeze(-2)
+        self.source_mask = (source != pad).unsqueeze(-2)
         if target is not None:
             self.target = target[:, :-1]
             self.target_y = target[:, 1:]
-            self.trg_mask = self.make_std_mask(self.target, pad)
+            self.target_mask = self.make_std_mask(self.target, pad)
             self.n_tokens = (self.target_y != pad).data.sum()
 
     @staticmethod
@@ -20,3 +21,30 @@ class Batch:
             subsequent_mask(target.size(-1)).type_as(target_mask.data)
         )
         return target_mask
+
+
+def train_step(data_iter, model, loss_function):
+    start = time()
+    total_tokens = 0
+    total_loss = 0
+    tokens = 0
+    for i, batch in enumerate(data_iter):
+        out = model.forward(
+            batch.source, batch.target,
+            batch.source_mask, batch.target_mask
+        )
+        loss = loss_function(
+            out, batch.target_y,
+            batch.n_tokens
+        )
+        total_loss += loss
+        total_tokens += batch.n_tokens
+        tokens += batch.n_tokens
+        if i % 50 == 1:
+            elapsed = time() - start
+            print(
+                "Epoch Step: %d Loss: %f Tokens per Sec: %f" %
+                (i, loss / batch.ntokens, tokens / elapsed))
+            start = time()
+            tokens = 0
+    return total_loss / total_tokens
