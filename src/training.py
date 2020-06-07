168d3d3b02cbd8ad898f26f876c974da48d4546c
soumik12345 <19soumik.rakshit96@gmail.com>
2020-06-08 00:53:19 +0530

    Added LabelSmoothing

--- src/training.py
+++ src/training.py
@@ -98,3 +98,29 @@ def get_optimizer(model):
             betas=(0.9, 0.98), eps=1e-9
         )
     )
+
+
+class LabelSmoothing(torch.nn.Module):
+
+    def __init__(self, size, padding_index, smoothing=0.0):
+        super(LabelSmoothing, self).__init__()
+        self.criterion = torch.nn.KLDivLoss(size_average=False)
+        self.padding_index = padding_index
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.size = size
+        self.true_dist = None
+
+    def forward(self, x, target):
+        assert x.size(1) == self.size
+        true_dist = x.data.clone()
+        true_dist.fill_(self.smoothing / (self.size - 2))
+        true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        true_dist[:, self.padding_index] = 0
+        mask = torch.nonzero(target.data == self.padding_idx)
+        if mask.dim() > 0:
+            true_dist.index_fill_(0, mask.squeeze(), 0.0)
+        self.true_dist = true_dist
+        return self.criterion(
+            x, torch.autograd.Variable(true_dist, requires_grad=False)
+        )
